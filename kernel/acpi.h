89d38be4352d94de80b86db4951ffbd125a2b937
wind3 <wind33333333@gmail.com>
2024-10-19 15:28:45 +0800

    20240823

--- kernel/acpi.h
+++ kernel/acpi.h
@@ -11,6 +11,7 @@ typedef struct {
 
 irq_to_gsi_t irq_to_gsi[24] = {0,0};
 
+//region acpi通用头
 typedef struct {
     CHAR8 signature[4];        // 表的签名，例如 "XSDT" "ACPI" "HPET" "MCFG"
     UINT32 length;             // 表的总长度，包括头部
@@ -22,21 +23,25 @@ typedef struct {
     UINT32 creator_id;         // 创建者的标识符
     UINT32 creator_revision;   // 创建者的修订版本
 } __attribute__((packed)) acpi_table_header_t;
+//endregion
 
+//region mcfg表
 typedef struct {
     UINT64 base_address;        // 配置空间基地址
     UINT64 pci_segment;         // PCIe 段号
     UINT64 start_bus_number;    // 起始总线号
     UINT64 end_bus_number;      // 结束总线号
     UINT64 reserved;            // 保留字段，通常为 0
-} mcfg_entry_t;
+}__attribute__((packed)) mcfg_entry_t;
 
 typedef struct {
     acpi_table_header_t header;                // 标准 ACPI 表头（36 字节）
     UINT64 reserved;                           // 保留字段（8 字节），应为 0
     mcfg_entry_t config_space_base[];          // PCIe 配置空间的区域描述符列表
 } mcfg_t;
+//endregion
 
+//region hpet表
 typedef struct {
     acpi_table_header_t header;         // 标准 ACPI 表头
     UINT32 event_timer_block_id;        // 定时器块的 ID
@@ -45,20 +50,59 @@ typedef struct {
     UINT16 minimum_tick;                // HPET 支持的最小时间间隔
     UINT8  page_protection;             // 页保护属性
 }__attribute__((packed)) hpett_t;
+//endregion
+
+//region madt表
+typedef struct {
+    UINT8    type;                 // 类型，0 表示本地 APIC
+    UINT8    length;               // 条目长度，通常为 8
+    UINT8    acpi_processor_id;    // ACPI 处理器 ID
+    UINT8    apic_id               // 本地 APIC ID
+    UINT32   flags;                // 启用状态（1 表示启用）
+}__attribute__((packed)) apic_entry;
+
+typedef struct {
+    UINT8   type;                           // 类型，1 表示 IO APIC
+    UINT8   length;                         // 条目长度，通常为 12
+    UINT8   ioapic_id;                      // IO APIC 的 ID
+    UINT8   reserved;                       // 保留，通常为 0
+    UINT32  ioapic_address;                 // IO APIC 的内存映射基地址
+    UINT32  global_system_interrupt_base;   // 全局中断号基址
+}__attribute__((packed)) ioapic_entry_t;
+
+typedef struct {
+    UINT8     type;                         // 类型，2 表示中断源重定向
+    UINT8     length;                       // 条目长度，通常为 10
+    UINT8     bus_source;                   // 总线源（通常为 0，表示 ISA 总线）
+    UINT8     irq_source;                   // ISA IRQ 号（0-15）
+    UINT32    global_system_interrupt;     // 重定向后的全局系统中断号
+    UINT16    flags;                       // 中断标志（边沿/电平触发，高/低电平等）
+} interrupt_source_override_entry_t;
+
+typedef struct {
+    UINT8      type;                        // 类型，4 表示 NMI 源
+    UINT8      length;                      // 条目长度，通常为 6
+    UINT16     flags;                       // 中断标志（边沿/电平触发等）
+    UINT32     global_system_interrupt;     // NMI 所关联的全局系统中断号
+} nmi_source_entry;
 
 typedef struct {
     acpi_table_header_t header;            // 标准 ACPI 表头
     UINT32 local_apic_address;             // 本地 APIC 的物理地址
     UINT32 flags;                          // 标志，表示系统支持哪些 APIC 功能
     // 接下来的部分是可变长度的 APIC 条目
-    UINT8 apic_structure[];                // APIC 条目数组
+    UINT8 apic_type[];                // APIC 条目数组
 }__attribute__((packed)) madt_t;
+//endregion
 
+//region xsdt表
 typedef struct {
     acpi_table_header_t header;          // 标准 ACPI 表头
     UINT32 *entry[];                     // 指向其他 ACPI 表的 64 位指针数组
 } __attribute__((packed)) xsdt_t;
+//endregion
 
+//region rsdp表
 typedef struct {
     CHAR8 signature[8];        // "RSD PTR "
     UINT8 checksum;            // 校验和
@@ -72,6 +116,7 @@ typedef struct {
     UINT8 extended_checksum;     // 扩展校验和，覆盖整个 RSDP 结构
     UINT8 reserved[3];           // 保留字段，必须为 0
 } __attribute__((packed)) rsdp_t;
+//endregion
 
 
 #endif
