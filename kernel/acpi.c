6ea3ee940f20ced0b78ef790aa158ae6b61312f9
wind3 <wind33333333@gmail.com>
2024-10-20 19:40:31 +0800

    20240823

--- kernel/acpi.c
+++ kernel/acpi.c
@@ -12,7 +12,7 @@ __attribute__((section(".init_text"))) void init_acpi(UINT8 bsp_flags) {
         hpett_t *hpett;
         mcfg_t *mcfg;
 
-        for (UINT32 i = 0; i < ((xsdt->acpi_header.length - sizeof(acpi_header_t)) / 4); i++) {
+        for (UINT32 i = 0; i < ((xsdt->acpi_header.length - sizeof(acpi_header_t)) / sizeof(UINT32 *)); i++) {
             if(*xsdt->entry[i]==0x43495041) {//"APIC"
                 madt = (madt_t *) xsdt->entry[i];
                 madt_header_t *madt_entry = (madt_header_t *)&madt->entry;
@@ -37,45 +37,12 @@ __attribute__((section(".init_text"))) void init_acpi(UINT8 bsp_flags) {
                 color_printk(RED,BLACK,"HPET MiniMumTick:%d Number:%d SpaceID:%d BitWidth:%d BiteOffset:%d AccessSize:%d Address:%#lX\n",hpett->minimum_tick,hpett->hpet_number,hpett->acpi_generic_adderss.space_id,hpett->acpi_generic_adderss.bit_width,hpett->acpi_generic_adderss.bit_offset,hpett->acpi_generic_adderss.access_size,hpett->acpi_generic_adderss.address);
             }else if(*xsdt->entry[i]==0x4746434D){//"MCFG"
                     mcfg = (mcfg_t *) xsdt->entry[i];
-                    mcfg_entry_t *mcfg_entry=(mcfg_entry *)&mcfg->entry;
-                    while(mcfg_entry<){
-
-                    }
-
-            }
-        }
-
-        /*    IOAPIC *ioapic = 0;
-            InterruptSourceOverride *isr = 0;
-            UINT32 j =0;
-            for (UINT32 i = 0; i < ((madt->Length - 44) / 2); i++) {
-                if ((madt->Header[i].Type == 1) && (madt->Header[i].Length == 0xC)) {
-                    ioapic = (IOAPIC *) &madt->Header[i];
-                    ioapic_baseaddr = LADDR_TO_HADDR((UINT32 *) ioapic->ioapic_address);
-                } else if ((madt->Header[i].Type == 2) && (madt->Header[i].Length == 0xA)) {
-                    isr = (InterruptSourceOverride *) &madt->Header[i];
-                    irq_to_gsi[j].IRQ = isr->Source;
-                    irq_to_gsi[j].GSI = isr->GlobalSystemInterrupt;
-                    j++;
+                    mcfg_entry_t *mcfg_entry=(mcfg_entry_t *)&mcfg->entry;
+                for(UINT32 i=0;i<((mcfg->acpi_header.length-sizeof(acpi_header_t)-sizeof(UINT64))/sizeof(mcfg_entry_t));i++){
+                    color_printk(RED,BLACK,"PCIE BaseAddr:%#lX Segment:%d StartBus:%d EndBus:%d\n",mcfg_entry[i].base_address,mcfg_entry[i].pci_segment,mcfg_entry[i].start_bus,mcfg_entry[i].end_bus);
                 }
             }
-
-            color_printk(YELLOW, BLACK, "RSDP: %#018lX \tRSDT: %#018lX\n", rsdp,
-                         rsdt);
-            color_printk(YELLOW, BLACK, "APIC: %#018lX \tIOAPIC ADDR: %#018lX\n", madt,
-                         ioapic_baseaddr);
-
-            color_printk(YELLOW, BLACK, "HPET: %#018lX \tHPET ADDR: %#018lX\n", hpet, hpet.address);
-
-            color_printk(YELLOW, BLACK, "MCFG: %#018lX \n", mcfg);
-
-            for (int i = 0; i < 24; ++i) {
-                if ((irq_to_gsi[i].IRQ == 0x0) && (irq_to_gsi[i].GSI == 0x0))
-                    break;
-                color_printk(YELLOW, BLACK, "IRQ#%d -> GSI#%d\n", irq_to_gsi[i].IRQ,
-                             irq_to_gsi[i].GSI);
-
-            }*/
+        }
     }
     return;
 }
