aee4b90fa91f3e1c6b57159b0b823e4de9c02a97
wind3 <wind33333333@gmail.com>
2024-11-11 22:28:10 +0800

    20240823

--- kernel/cpu.c
+++ kernel/cpu.c
@@ -21,9 +21,9 @@ void init_cpu(void){
     init_tss();                                //初始化TSS
     init_idt();                                //初始化IDT
     init_apic();                               //初始化apic
-    init_ap();                                 //初始化ap核
     color_printk(GREEN, BLACK, "CPU Manufacturer: %s  Model: %s\n",cpu_info.manufacturer_name, cpu_info.model_name);
     color_printk(GREEN, BLACK, "CPU Cores: %d  FundamentalFrequency: %ldMhz  MaximumFrequency: %ldMhz  BusFrequency: %ldMhz  TSCFrequency: %ldhz\n",cpu_info.logical_processors_number,cpu_info.fundamental_frequency,cpu_info.maximum_frequency,cpu_info.bus_frequency,cpu_info.tsc_frequency);
+    init_ap();                                 //初始化ap核
     color_printk(GREEN, BLACK, "CPUID:%d APICID:%d init successful\n", cpu_id,apic_id);
     return;
 }
--- kernel/tss.c
+++ kernel/tss.c
@@ -13,10 +13,12 @@ __attribute__((section(".init_text"))) void init_tss(void) {
     for (int i = 0; i < cpu_info.logical_processors_number; i++) {
         tss_ptr[i].reserved0 = 0;
         tss_ptr[i].rsp0 = (UINT64) LADDR_TO_HADDR(alloc_pages(4) + PAGE_4K_SIZE * 4);
+        map_pages(HADDR_TO_LADDR(tss_ptr[i].rsp0)-PAGE_4K_SIZE * 4,tss_ptr[i].rsp0-PAGE_4K_SIZE * 4,4,PAGE_ROOT_RW);
         tss_ptr[i].rsp1 = 0;
         tss_ptr[i].rsp2 = 0;
         tss_ptr[i].reserved1 = 0;
         tss_ptr[i].ist1 = (UINT64) LADDR_TO_HADDR(alloc_pages(4) + PAGE_4K_SIZE * 4);
+        map_pages(HADDR_TO_LADDR(tss_ptr[i].ist1)-PAGE_4K_SIZE * 4,tss_ptr[i].ist1-PAGE_4K_SIZE * 4,4,PAGE_ROOT_RW);
         tss_ptr[i].ist2 = 0;
         tss_ptr[i].ist3 = 0;
         tss_ptr[i].ist4 = 0;
@@ -29,7 +31,6 @@ __attribute__((section(".init_text"))) void init_tss(void) {
 
         //设置gdt tss描述符
         SET_TSS(gdt_ptr.base,TSS_DESCRIPTOR_START_INDEX+ i,tss_ptr + i);
-        memory_management.kernel_end_address = tss_ptr[i].ist1;
     }
     LTR(TSS_DESCRIPTOR_START_INDEX * 8);
     return;
