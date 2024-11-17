8266ac40adebdeb27b0927aa5f2c7e86b1031af4
wind3 <wind33333333@gmail.com>
2024-11-17 12:54:18 +0800

    20240823

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -5,7 +5,7 @@ __attribute__((section(".init_text"))) void init_syscall(void){
     //执行syscall指令时 CS=IA32_STAR_MSR[bit32-bit47]&0xFFFC  SS=IA32_STAR_MSR[bit32-bit47]&0xFFFC+8
     //执行sysret执行返回64位代码段时 CS=IA32_STAR_MSR[bit48-bit63]&0xFFFC+16|3  SS=IA32_STAR_MSR[bit48-bit63]&0xFFFC+8|3
     //执行sysret执行返回32位代码段时 CS=IA32_STAR_MSR[bit48-bit63]&0xFFFC|3  SS=IA32_STAR_MSR[bit48-bit63]&0xFFFC+8|3
-    WRMSR(IA32_STAR_MSR,((0x18UL<<48)|(0x8UL<<32)));
+    WRMSR(IA32_STAR_MSR,(0x18UL<<48)|(0x8UL<<32));
 
     //RIP=IA32_LSTAR_MSR  长模式系统调用入口地址
     WRMSR(IA32_LSTAR_MSR,long_mode_syscall_entry);
