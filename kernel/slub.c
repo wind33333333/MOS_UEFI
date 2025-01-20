93b8fc800937e4c9524e1b3a91dd32bfc4c8e8be
wind3 <wind33333333@gmail.com>
2025-01-20 21:06:39 +0800

    20240823

--- kernel/slub.c
+++ kernel/slub.c
@@ -88,27 +88,26 @@ void kmem_cache_free(kmem_cache_t *cache, void *object) {
     void* align_addr = (void*)((UINT64)object & (PAGE_4K_MASK << cache->order_per_slub));
 
     for (UINT32 i = 0; i < cache->slub_count; i++) {
-        if (align_addr != cache_node->object_start_vaddr) {
-            cache_node = (kmem_cache_node_t*)cache_node->slub_node.next;
-            continue;
-        }
-        cache_node->free_count++;
-        cache_node->using_count--;
-        cache->total_free++;
-        cache->total_using--;
-        *(UINT64*)object = cache_node->free_list;
-        cache_node->free_list = object;
+        if (align_addr == cache_node->object_start_vaddr) {
+            cache_node->free_count++;
+            cache_node->using_count--;
+            cache->total_free++;
+            cache->total_using--;
+            *(UINT64*)object = (UINT64)cache_node->free_list;
+            cache_node->free_list = object;
 
-        //如果当前slub所有对象已经释放，且cache总空闲对象大于slub对象则释放当前的slub到伙伴系统
-        if (cache_node->using_count == 0 && cache->total_free > cache->object_per_slub) {
-            UINT64* vir_addr = vaddr_to_pte_vaddr(cache_node->object_start_vaddr);
-            UINT64 phy_addr = *vir_addr & 0x7FFFFFFFFFFFF000UL;
-            page_t* page = phyaddr_to_page(phy_addr);
-            buddy_free_pages(page);
-            kmem_cache_free(&node_kmem_cache,cache_node);
-            list_del(&cache_node->slub_node);
+            //如果当前slub所有对象已经释放，且cache总空闲对象大于一个slub对象数量，则释放当前的slub到伙伴系统
+            if (cache_node->using_count == 0 && cache->total_free > cache->object_per_slub) {
+                UINT64* vir_addr = vaddr_to_pte_vaddr(cache_node->object_start_vaddr);
+                UINT64 phy_addr = *vir_addr & 0x7FFFFFFFFFFFF000UL;
+                page_t* page = phyaddr_to_page(phy_addr);
+                buddy_free_pages(page);
+                kmem_cache_free(&node_kmem_cache,cache_node);
+                list_del((list_head_t*)cache_node);
+            }
+            return;
         }
-        return;
+        cache_node = (kmem_cache_node_t*)cache_node->slub_node.next;
     }
 }
 
