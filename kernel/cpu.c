e6cc25c0ad018f64a5940669b87ccedd278a77f2
wind3 <wind33333333@gmail.com>
2025-02-08 22:40:07 +0800

    20240823

--- kernel/cpu.c
+++ kernel/cpu.c
@@ -18,7 +18,6 @@ INIT_TEXT void init_cpu(void){
     cpu_id = apicid_to_cpuid(apic_id);         //获取cpu_id
     init_cpu_amode();                          //初始化cpu开启高级功能
     get_cpu_info();                            //获取cpu信息
-    init_kpage_table;                          //初始化内核页表
     init_gdt();                                //初始化GDT
     init_tss();                                //初始化TSS
     init_idt();                                //初始化IDT
