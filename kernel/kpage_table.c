2cbb17b8c51e25614a73b226dcc16f4386e04f83
wind3 <wind33333333@gmail.com>
2025-02-13 18:05:29 +0800

    20250209

--- kernel/kpage_table.c
+++ kernel/kpage_table.c
@@ -5,28 +5,34 @@
 #include "memblock.h"
 #include "vmm.h"
 
-INIT_DATA UINT64* kpml4t_ptr;          //正式内核页表
+INIT_DATA UINT64 *kpml4t_ptr; //正式内核页表
 
 INIT_TEXT void init_kpage_table(void) {
     kpml4t_ptr = memblock_alloc(PAGE_4K_SIZE, PAGE_4K_SIZE);
     mem_set(kpml4t_ptr, 0, PAGE_4K_SIZE);
 
-
-    memblock_vmmap1(PA_TO_VA(kpml4t_ptr),0,0xFFFF807FFFFFF000,2,PAGE_ROOT_RWX);
-
-
+    //直接映射区
+    memblock_vmmap_huge(kpml4t_ptr, 0,DIRECT_MAP_OFFSET,
+                        memblock.memory.region[memblock.memory.count - 1].base + memblock.memory.region[
+                            memblock.memory.count - 1].size,PAGE_ROOT_RW);
     //.init_text-.init_data 可读写执行
-    memblock_vmmap(VA_TO_PA(_start_init_text), (void*)VA_TO_PA(_start_init_text), _start_text-_start_init_text >> PAGE_4K_SHIFT, PAGE_ROOT_RWX);
+    memblock_vmmap1(kpml4t_ptr,_start_init_text-KERNEL_OFFSET, _start_init_text, _start_text - _start_init_text,
+                    PAGE_ROOT_RWX);
     //.text可读执行
-    memblock_vmmap(VA_TO_PA(_start_text), (void*)VA_TO_PA(_start_text), _start_data-_start_text >> PAGE_4K_SHIFT, PAGE_ROOT_RX);
+    memblock_vmmap1(kpml4t_ptr,_start_text-KERNEL_OFFSET, _start_text, _start_data - _start_text, PAGE_ROOT_RX);
     //.data-.stack可读写
-    memblock_vmmap(VA_TO_PA(_start_data), (void*)VA_TO_PA(_start_data), _end_stack-_start_data >> PAGE_4K_SHIFT, PAGE_ROOT_RW);
+    memblock_vmmap1(kpml4t_ptr,_start_data-KERNEL_OFFSET, _start_data, _end_stack - _start_data, PAGE_ROOT_RW);
+
+    //memblock_vmmap1(PA_TO_VA(kpml4t_ptr),0,0xFFFF807FFFFFF000,2,PAGE_ROOT_RWX);
+
+
+    // //.init_text-.init_data 可读写执行
+    // memblock_vmmap(VA_TO_PA(_start_init_text), (void*)VA_TO_PA(_start_init_text), _start_text-_start_init_text >> PAGE_4K_SHIFT, PAGE_ROOT_RWX);
+    // //.text可读执行
+    // memblock_vmmap(VA_TO_PA(_start_text), (void*)VA_TO_PA(_start_text), _start_data-_start_text >> PAGE_4K_SHIFT, PAGE_ROOT_RX);
+    // //.data-.stack可读写
+    // memblock_vmmap(VA_TO_PA(_start_data), (void*)VA_TO_PA(_start_data), _end_stack-_start_data >> PAGE_4K_SHIFT, PAGE_ROOT_RW);
 
     //设置正式内核页表
     set_cr3(kpml4t_ptr);
 }
-
-
-
-
-
