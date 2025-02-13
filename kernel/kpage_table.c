04104ad5231fb33b872cb878bb2d164d994322ff
wind3 <wind33333333@gmail.com>
2025-02-13 18:21:46 +0800

    20250209

--- kernel/kpage_table.c
+++ kernel/kpage_table.c
@@ -25,14 +25,6 @@ INIT_TEXT void init_kpage_table(void) {
 
     //memblock_vmmap1(PA_TO_VA(kpml4t_ptr),0,0xFFFF807FFFFFF000,2,PAGE_ROOT_RWX);
 
-
-    // //.init_text-.init_data 可读写执行
-    // memblock_vmmap(VA_TO_PA(_start_init_text), (void*)VA_TO_PA(_start_init_text), _start_text-_start_init_text >> PAGE_4K_SHIFT, PAGE_ROOT_RWX);
-    // //.text可读执行
-    // memblock_vmmap(VA_TO_PA(_start_text), (void*)VA_TO_PA(_start_text), _start_data-_start_text >> PAGE_4K_SHIFT, PAGE_ROOT_RX);
-    // //.data-.stack可读写
-    // memblock_vmmap(VA_TO_PA(_start_data), (void*)VA_TO_PA(_start_data), _end_stack-_start_data >> PAGE_4K_SHIFT, PAGE_ROOT_RW);
-
     //设置正式内核页表
     set_cr3(kpml4t_ptr);
 }
