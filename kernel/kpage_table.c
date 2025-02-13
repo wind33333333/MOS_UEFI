3d4e2718da2e716bdf65494912651c0d8681d593
wind3 <wind33333333@gmail.com>
2025-02-13 11:27:17 +0800

    20250209

--- kernel/kpage_table.c
+++ kernel/kpage_table.c
@@ -16,25 +16,31 @@ INIT_TEXT void init_kpage_table(void) {
     mem_set(kpml4t_ptr, 0, PAGE_4K_SIZE);
 
     //直接映射区
-    pml4e_count = memblock.memory.region[memblock.memory.count-1].base+memblock.memory.region[memblock.memory.count-1].size+0x7FFFFFFFFF >> 39;
-    pdpte_count = memblock.memory.region[memblock.memory.count-1].base+memblock.memory.region[memblock.memory.count-1].size+0x3FFFFFFF >> 30;
+    UINT64 page_count = memblock.memory.region[memblock.memory.count-1].base+memblock.memory.region[memblock.memory.count-1].size >> PAGE_4K_SHIFT;
+    pml4e_count = calculate_pml4e_count((void*)DIRECT_MAP_OFFSET,page_count);
+    pdpte_count = calculate_pdpte_count((void*)DIRECT_MAP_OFFSET,page_count);
     pml4t = kpml4t_ptr+(DIRECT_MAP_OFFSET>>39&0x1ff);
     phy_addr=0|PAGE_ROOT_RW;
     for (UINT32 i = 0; i < pml4e_count; i++) {
         pdptt = memblock_alloc(PAGE_4K_SIZE,PAGE_4K_SIZE);
         mem_set(pdptt, 0, PAGE_4K_SIZE);
         pml4t[i] = (UINT64)pdptt | PAGE_RW | PAGE_P;
-        for (UINT32 j = 0; j < pdpte_count; j++) {
-            pdptt[j] = phy_addr;
-            phy_addr += PAGE_4K_SIZE;
-        }
+    }
+
+    UINT32 j =0;
+    for (UINT32 i = 0; i < pdpte_count; i++) {
+        pdptt = pml4t[j]&0x7FFFFFFFF000;
+        pdptt[i] = phy_addr;
+        phy_addr += 0x40000000;
+        if (i == 512) j++;
     }
 
     //内核映射区
-    pml4e_count = _end - _start + 0x7FFFFFFFFF >> 39;
-    pdpte_count = _end - _start + 0x3FFFFFFF >> 30;
-    pde_count = _end - _start + 0x1FFFFF >> 21;
-    pte_count = _end - _start + 0xFFF >> 12;
+    page_count = _end - _start >> PAGE_4K_SHIFT;
+    pml4e_count = calculate_pml4e_count(_start,page_count);
+    pdpte_count = calculate_pdpte_count(_start,page_count);
+    pde_count = calculate_pde_count(_start,page_count);
+    pte_count = page_count;
     pml4t = kpml4t_ptr+(KERNEL_OFFSET>>39&0x1ff);
     for (UINT32 i = 0; i < pml4e_count; i++) {
         pdptt = memblock_alloc(PAGE_4K_SIZE,PAGE_4K_SIZE);
