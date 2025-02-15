fe2b0fbf575f1b0b0410b24480639cb17d9db680
wind3 <wind33333333@gmail.com>
2025-02-15 12:17:17 +0800

    20250209

--- kernel/vmm.c
+++ kernel/vmm.c
@@ -1,10 +1,11 @@
 #include "vmm.h"
 
+#include <stdbool.h>
 #include <strings.h>
 
 #include "buddy_system.h"
 
-void vmmap(UINT64 *pml4t, UINT64 pa, void *va, UINT64 attr) {
+BOOLEAN vmmap(UINT64 *pml4t, UINT64 pa, void *va, UINT64 attr) {
     UINT64 *pdptt, *pdt, *ptt;
     UINT32 index;
     UINT64 page_size = attr >> 7 & 5; //取attr中的第7位和第9位 0=4K 1=2M 5=1G
@@ -18,10 +19,13 @@ void vmmap(UINT64 *pml4t, UINT64 pa, void *va, UINT64 attr) {
 
     pdptt = pa_to_va(pml4t[index] & 0x7FFFFFFFF000);
     index = get_pdpte_index(va);
-    if (pdptt[index] == 0 && page_size == 5) {//1G页
-        pdptt[index] = pa | attr;
-        invlpg(va);
-        return;
+    if (page_size == 5) {//1G页
+        if (pdptt[index] == 0) {
+            pdptt[index] = pa | attr;
+            invlpg(va);
+            return FALSE;    //1G页映射成功
+        }
+        return TRUE;   //已被占用
     }
 
     if (pdptt[index] == 0) {
@@ -31,10 +35,13 @@ void vmmap(UINT64 *pml4t, UINT64 pa, void *va, UINT64 attr) {
 
     pdt = pa_to_va(pdptt[index] & 0x7FFFFFFFF000);
     index = get_pde_index(va);
-    if (pdt[index] == 0 && page_size == 1) {//2M页
-        pdt[index] = pa | attr;
-        invlpg(va);
-        return;
+    if (page_size == 1) {//2M页
+        if (pdt[index] == 0) {
+            pdt[index] = pa | attr;
+            invlpg(va);
+            return FALSE;  //2M页映射成功
+        }
+        return TRUE;      //以占用
     }
 
     if (pdt[index] == 0) {
@@ -46,9 +53,10 @@ void vmmap(UINT64 *pml4t, UINT64 pa, void *va, UINT64 attr) {
     index = get_pte_index(va);
     if (ptt[index] == 0) {
         ptt[index] = pa | attr;
+        invlpg(va);
+        return FALSE;           //4K页映射成功
     }
-
-    invlpg(va);
+    return TRUE;                //失败
 }
 
 void vmunmap(UINT64 *pml4t, void *va) {
--- kernel/vmm.h
+++ kernel/vmm.h
@@ -184,8 +184,8 @@ void bitmap_free_pages(UINT64 pa, UINT64 page_count);
 void *bitmap_map_pages(UINT64 pa, void *va, UINT64 page_count, UINT64 attr);
 void bitmap_unmap_pages(void *va, UINT64 page_count);
 
-void vmmap(UINT64 *pml4t, UINT64 pa, void *va, UINT64 attr);
-void vmunmap(UINT64 *pml4t, void *va);
+BOOLEAN vmmap(UINT64 *pml4t, UINT64 pa, void *va, UINT64 attr);
+BOOLEAN vmunmap(UINT64 *pml4t, void *va);
 
 
 #endif
