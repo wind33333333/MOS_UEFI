9686845b43de848c727acc0a04762a208b53b93d
wind3 <wind33333333@gmail.com>
2025-02-16 17:12:34 +0800

    20250209

--- kernel/buddy_system.h
+++ kernel/buddy_system.h
@@ -23,22 +23,22 @@ typedef struct buddy_system_t {
 
 extern buddy_system_t buddy_system;
 
-//page转换物理地址
+//page地址转换物理地址
 static inline UINT64 page_to_pa(page_t *page) {
     return (UINT64)(page - buddy_system.page_table) << PAGE_4K_SHIFT;
 }
 
-//物理地址转换page
+//物理地址转换page地址
 static inline page_t* pa_to_page(UINT64 pa) {
     return buddy_system.page_table+(pa >> PAGE_4K_SHIFT);
 }
 
-//page转虚拟地址
+//page地址转虚拟地址
 static inline void *page_to_va(page_t *page) {
     return pa_to_va(page_to_pa(page));
 }
 
-//虚拟地址转page
+//虚拟地址转page地址
 static inline page_t *va_to_page(void *va) {
     return pa_to_page(va_to_pa(va));
 }
