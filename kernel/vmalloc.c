0ebe30d0ebc1f74efd3217e2cfafd26e7ab551fc
wind3 <wind33333333@gmail.com>
2025-03-15 20:26:30 +0800

    20250209

--- kernel/vmalloc.c
+++ kernel/vmalloc.c
@@ -79,14 +79,17 @@ void right_rotate(rbtree_t *rbtree, rbtree_node_t *root) {
 void rb_insert_color(rbtree_t *rbtree, rbtree_node_t *cur) {
     rbtree_node_t *uncle;
     while (cur->parent && cur->parent->color == red_node) {
-        if (cur->parent == cur->parent->parent->left) {//LXX
+        if (cur->parent == cur->parent->parent->left) {
+            //LXX
             uncle = cur->parent->parent->right;
-            if (uncle && uncle->color == red_node) {//LXR
+            if (uncle && uncle->color == red_node) {
+                //LXR
                 cur->parent->color = black_node;
                 uncle->color = black_node;
                 cur->parent->parent->color = red_node;
                 cur = cur->parent->parent;
-            } else if (!uncle || uncle->color == black_node) {//LXB
+            } else if (!uncle || uncle->color == black_node) {
+                //LXB
                 if (cur == cur->parent->right) {
                     //LRB
                     cur = cur->parent;
@@ -96,14 +99,17 @@ void rb_insert_color(rbtree_t *rbtree, rbtree_node_t *cur) {
                 cur->parent->parent->color = red_node;
                 right_rotate(rbtree, cur->parent->parent);
             }
-        } else if (cur->parent == cur->parent->parent->right) {//RXX
+        } else if (cur->parent == cur->parent->parent->right) {
+            //RXX
             uncle = cur->parent->parent->left;
-            if (uncle && uncle->color == red_node) {//RXR
+            if (uncle && uncle->color == red_node) {
+                //RXR
                 cur->parent->color = black_node;
                 uncle->color = black_node;
                 cur->parent->parent->color = red_node;
                 cur = cur->parent->parent;
-            } else if (!uncle || uncle->color == black_node) {//RXB
+            } else if (!uncle || uncle->color == black_node) {
+                //RXB
                 if (cur == cur->parent->left) {
                     //RLB
                     cur = cur->parent;
@@ -183,47 +189,50 @@ void mid_traversal1(rbtree_t *rbtree) {
  * parent:  node的父节点
  * root:    树的根节点
  */
-static void rb_erase_color(rbtree_t *rbtree,rbtree_node_t *node, rbtree_node_t *parent) {
+static void rb_erase_color(rbtree_t *rbtree, rbtree_node_t *node, rbtree_node_t *parent) {
     rbtree_node_t *sibling;
 
     // 循环处理，直到node是根节点或node变为红色
-    while (node != rbtree->root && (node==NULL || node->color == black_node)) {
+    while (node != rbtree->root && (node == NULL || node->color == black_node)) {
+        // 当前节点是父节点的右子节点，兄弟节点为父节点的左子节点
         if (node == parent->right) {
-            // 当前节点是父节点的右子节点，兄弟节点为父节点的左子节点
             sibling = parent->left;
+
+            /* ------------- Case 1: 兄弟节点为红色 ------------- */
+            if (sibling->color == red_node) {
+                //兄变黑父变红，父亲右旋,兄弟节点变成原兄弟的右孩
+                sibling->color = black_node;
+                parent->color = red_node;
+                right_rotate(rbtree, parent);
+                sibling = parent->left;
+            }
+
+            /* ---- Case 2: 兄弟节点为黑---- */
             if (sibling->color == black_node) {
-                //兄弟是黑色
-                if ((sibling->left==NULL || sibling->left->color == black_node) && (sibling->right->color == black_node || sibling->right==NULL)) {
-                    //孩子是黑色,兄弟染红，双黑上移
+                //case2.1 兄弟的两个孩子也是黑色
+                if ((sibling->left == NULL && sibling->right == NULL) {
+                    //兄弟染红，双黑上移
                     sibling->color = red_node;
                     node = parent;
                     parent = parent->parent;
-                    if (parent->color == red_node)parent->color = black_node;
-                    break;
-                }else {
-                    //孩子的颜色是红色
+
+                }else{//case2.2 兄弟节的孩子是红色
+                    //左兄的右孩是红色 LRR型
                     if (sibling->left == NULL && sibling->right->color == red_node) {
-                        //左兄的右孩是红色 LRR型
-                        left_rotate(rbtree, sibling);   //左旋兄弟
+                        left_rotate(rbtree, sibling); //左旋兄弟
                         sibling = parent->left;
                     }
                     //左兄的左孩是红色  LLR型
                     sibling->color = parent->color; //兄弟继承父亲颜色，父亲和左孩变黑
                     parent->color = black_node;
                     sibling->left->color = black_node;
-                    right_rotate(rbtree, parent);  //右旋父亲
+                    right_rotate(rbtree, parent); //右旋父亲
                     break;
                 }
-            }else {
-                //兄弟为红色,兄变黑父变红，父亲右旋
-                sibling->color = black_node;
-                parent->color = red_node;
-                right_rotate(rbtree, parent);
             }
         }
     }
     mid_traversal1(rbtree);
-
 }
 
 /*
@@ -233,68 +242,72 @@ static void rb_erase_color(rbtree_t *rbtree,rbtree_node_t *node, rbtree_node_t *
 void rbtree_delete(rbtree_t *rbtree, UINT64 key) {
     //通过key查找node
     rbtree_node_t *del_node = rbtree->root;
-    while (del_node ) {
-        if (del_node ->key == key) break; //搜索key对应的节点
-        del_node  = key < del_node ->key ? del_node ->left : del_node ->right;
+    while (del_node) {
+        if (del_node->key == key) break; //搜索key对应的节点
+        del_node = key < del_node->key ? del_node->left : del_node->right;
     }
     if (!del_node) return; //没有找到
 
     /**********************************************/
 
-    rbtree_node_t *parent;           // 父亲节点
-    rbtree_node_t *child;            // 被删除节点的孩子节点
-    int color;                       // 被删除节点的原始颜色
+    rbtree_node_t *parent; // 父亲节点
+    rbtree_node_t *child; // 被删除节点的孩子节点
+    int color; // 被删除节点的原始颜色
 
-    if (del_node->left && del_node->right) { //情况1：删除节点左右子树都有，找后继节点
+    if (del_node->left && del_node->right) {
+        //情况1：删除节点左右子树都有，找后继节点
         rbtree_node_t *successor = del_node->right;
-        while (successor->left) successor = successor->left;     //找后继
+        while (successor->left) successor = successor->left; //找后继
 
         parent = successor;
-        child = successor->right;       // 后继节点的右子节点（可能为空）
-        color = successor->color;       // 后继节点的颜色
+        child = successor->right; // 后继节点的右子节点（可能为空）
+        color = successor->color; // 后继节点的颜色
 
-        if (successor != del_node->right) {       // 后继节点不是原始节点的直接右子节点
+        if (successor != del_node->right) {
+            // 后继节点不是原始节点的直接右子节点
             parent = successor->parent;
-            if (child) child->parent=parent;      // 如果后继节点有右子节点,更新右子节点的父指针
-            parent->left = child;                 // 后继节点父亲的左子节点，更新为后继节点的右子节点
-            successor->right = del_node->right;                     // 后继节点的右子节点，更新为原始节点右孩
-            del_node->right->parent = successor;                    // 原色节点的右子节点父指针更新为后继节点
+            if (child) child->parent = parent; // 如果后继节点有右子节点,更新右子节点的父指针
+            parent->left = child; // 后继节点父亲的左子节点，更新为后继节点的右子节点
+            successor->right = del_node->right; // 后继节点的右子节点，更新为原始节点右孩
+            del_node->right->parent = successor; // 原色节点的右子节点父指针更新为后继节点
         }
-        successor->left = del_node->left;                      // 后继节点的左子节点，更新为原始节点的左孩
-        del_node->left->parent = successor;                    // 原始节点左子节点的父亲，更新为后继节点
-        successor->parent = del_node->parent;                  // 后继节点的父节点，更新为原始节点的父节点
-        successor->color = del_node->color;                    // 后继节点继承原始节点颜色
+        successor->left = del_node->left; // 后继节点的左子节点，更新为原始节点的左孩
+        del_node->left->parent = successor; // 原始节点左子节点的父亲，更新为后继节点
+        successor->parent = del_node->parent; // 后继节点的父节点，更新为原始节点的父节点
+        successor->color = del_node->color; // 后继节点继承原始节点颜色
 
         if (del_node->parent) {
             if (del_node == del_node->parent->left) {
-                del_node->parent->left=successor;                  // 原始节点为父节点左孩，更新为后继节点
-            }else {
-                del_node->parent->right=successor;                 // 原始节点为父节点右孩，更新为后继节点
+                del_node->parent->left = successor; // 原始节点为父节点左孩，更新为后继节点
+            } else {
+                del_node->parent->right = successor; // 原始节点为父节点右孩，更新为后继节点
             }
         } else {
-            rbtree->root = successor;                          // 更新根节点
+            rbtree->root = successor; // 更新根节点
         }
-
-    }else { //情况2：只有1个子树或0个子树
+    } else {
+        //情况2：只有1个子树或0个子树
         child = del_node->left ? del_node->left : del_node->right;
-        parent = del_node->parent;          //被删除节点的父亲
-        color = del_node->color;            //被删除节点颜色
+        parent = del_node->parent; //被删除节点的父亲
+        color = del_node->color; //被删除节点颜色
 
-        if (child) child->parent = parent;  //有孩子则更新孩子的父亲指针为删除节点的父亲
-        if (parent) {                       //被删除节点有父亲
-            if (del_node == parent->left) { //被删除节点为父亲的左孩
-                parent->left = child;       //把孩子更新为父亲的左孩
-            }else {
-                parent->right = child;      //把孩子更新为父亲的右孩
+        if (child) child->parent = parent; //有孩子则更新孩子的父亲指针为删除节点的父亲
+        if (parent) {
+            //被删除节点有父亲
+            if (del_node == parent->left) {
+                //被删除节点为父亲的左孩
+                parent->left = child; //把孩子更新为父亲的左孩
+            } else {
+                parent->right = child; //把孩子更新为父亲的右孩
             }
-        }else {                             //被删除节点无父亲则是根节点
-            rbtree->root = child;           //把孩子更新为新的根节点
+        } else {
+            //被删除节点无父亲则是根节点
+            rbtree->root = child; //把孩子更新为新的根节点
         }
     }
 
     mid_traversal1(rbtree);
     if (color == black_node) rb_erase_color(rbtree, child, parent);
-
 }
 
 //递归中序遍历
@@ -308,7 +321,7 @@ void mid_traversal(rbtree_t *rbtree, rbtree_node_t *node) {
 
 void rb_test(void) {
     //红黑树测试
-    UINT64 keyare[34] = {83, 22, 99, 35, 95, 78, 75, 92, 40, 76, 93,41};
+    UINT64 keyare[34] = {83, 22, 99, 35, 95, 78, 75, 92, 40, 76, 93, 41};
     rbtree_t *rbtree = kmalloc(sizeof(rbtree_t));
     rbtree->root = NULL;
     rbtree_node_t *node = NULL;
