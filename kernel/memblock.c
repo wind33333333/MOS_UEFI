5d3ea0dfbfd9287e15537c7f3d4f5d20eb0165f2
wind3 <wind33333333@gmail.com>
2025-05-27 19:39:46 +0800

    20250209

--- kernel/memblock.c
+++ kernel/memblock.c
@@ -3,22 +3,36 @@
 #include "vmm.h"
 
 INIT_DATA memblock_t memblock;
+INIT_DATA memblock_type_t phy_mem_map;
 
 INIT_TEXT void init_memblock(void) {
     boot_info = pa_to_va(boot_info);
     boot_info->mem_map = pa_to_va(boot_info->mem_map);
     for (UINT32 i = 0; i < (boot_info->mem_map_size / boot_info->mem_descriptor_size); i++) {
         //如果内存类型是1M内或是lode_data或是acpi则先放入保留区
-        if ((boot_info->mem_map[i].PhysicalStart < 0x100000 && boot_info->mem_map[i].NumberOfPages != 0) || boot_info->
-            mem_map[i].Type == EFI_LOADER_DATA || boot_info->mem_map[i].Type == EFI_ACPI_RECLAIM_MEMORY) {
+        if ((boot_info->mem_map[i].PhysicalStart < 0x100000 && boot_info->mem_map[i].NumberOfPages != 0) ||\
+            boot_info->mem_map[i].Type == EFI_LOADER_DATA ||\
+            boot_info->mem_map[i].Type == EFI_ACPI_RECLAIM_MEMORY) {
             memblock_add(&memblock.reserved, boot_info->mem_map[i].PhysicalStart,
                          boot_info->mem_map[i].NumberOfPages << PAGE_4K_SHIFT);
             //其他可用类型合并放入可用类型保存
-        } else if (boot_info->mem_map[i].Type == EFI_LOADER_CODE || boot_info->mem_map[i].Type == EFI_BOOT_SERVICES_CODE
-                   || boot_info->mem_map[i].Type == EFI_BOOT_SERVICES_DATA || boot_info->mem_map[i].Type ==
-                   EFI_CONVENTIONAL_MEMORY) {
+        } else if (boot_info->mem_map[i].Type == EFI_LOADER_CODE ||\
+            boot_info->mem_map[i].Type == EFI_BOOT_SERVICES_CODE ||\
+            boot_info->mem_map[i].Type == EFI_BOOT_SERVICES_DATA ||\
+            boot_info->mem_map[i].Type == EFI_CONVENTIONAL_MEMORY) {
             memblock_add(&memblock.memory, boot_info->mem_map[i].PhysicalStart,
                          boot_info->mem_map[i].NumberOfPages << PAGE_4K_SHIFT);
+        }
+        //把所有物理内存放入phy_mem_map
+        if (boot_info->mem_map[i].NumberOfPages != 0 &&\
+        (boot_info->mem_map[i].Type == EFI_LOADER_DATA ||\
+        boot_info->mem_map[i].Type == EFI_LOADER_CODE ||\
+        boot_info->mem_map[i].Type == EFI_BOOT_SERVICES_CODE ||\
+        boot_info->mem_map[i].Type == EFI_BOOT_SERVICES_DATA ||\
+        boot_info->mem_map[i].Type == EFI_CONVENTIONAL_MEMORY ||\
+        boot_info->mem_map[i].Type == EFI_ACPI_RECLAIM_MEMORY)) {
+            memblock_add(&phy_mem_map, boot_info->mem_map[i].PhysicalStart,
+             boot_info->mem_map[i].NumberOfPages << PAGE_4K_SHIFT);
         }
     }
 }
