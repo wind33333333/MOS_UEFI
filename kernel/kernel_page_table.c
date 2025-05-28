d4b6708bd02b2a0c1fa82174c1acab2dc5df57c9
wind3 <wind33333333@gmail.com>
2025-05-28 21:13:00 +0800

    20250209

--- kernel/kernel_page_table.c
+++ kernel/kernel_page_table.c
@@ -10,23 +10,23 @@ INIT_TEXT void init_kpage_table(void) {
     kpml4t_ptr = memblock_alloc(PAGE_4K_SIZE, PAGE_4K_SIZE);
     mem_set(kpml4t_ptr, 0, PAGE_4K_SIZE);
     //虚拟地址和物理地址低4G空间左对等映射
-    memblock_mmap_range(kpml4t_ptr, 0,(void*)0,4*40000000,PAGE_ROOT_RWX_2M1G,PAGE_1G_SIZE);
+    memblock_mmap_range(kpml4t_ptr, 0,(void*)0,4*PAGE_1G_SIZE,PAGE_ROOT_RWX_2M1G,PAGE_1G_SIZE);
     //直接映射区
     memblock_mmap_range(kpml4t_ptr, 0,DIRECT_MAP_OFFSET,
                         memblock.memory.region[memblock.memory.count - 1].base + memblock.memory.region[
                             memblock.memory.count - 1].size,PAGE_ROOT_RW_2M1G,PAGE_1G_SIZE);
     //初始化vmemmap区为2M页表
     for (UINT32 i = 0; i < phy_mem_map.count; i++) {
-        UINT64 pa = phy_mem_map.region[i].base;
+        UINT64 base = phy_mem_map.region[i].base;
         UINT64 size = phy_mem_map.region[i].size;
-        UINT64 vmemmap_va = (UINT64)pa_to_page(pa)&PAGE_2M_MASK;
+        UINT64 vmemmap_va = (UINT64)pa_to_page(base)&PAGE_2M_MASK;
         UINT64 pdte_count = PAGE_2M_ALIGN((size >> PAGE_4K_SHIFT)*sizeof(page_t))>>PAGE_2M_SHIFT;
         for (UINT64 i = 0; i < pdte_count; i++) {
             if (find_page_table_entry(kpml4t_ptr, vmemmap_va, pde_level)) {
                 vmemmap_va += PAGE_2M_SIZE;
                 continue;
             }
-            UINT64 pa = (UINT64)memblock_alloc(PAGE_2M_SIZE,PAGE_2M_SIZE);
+            UINT64 pa = memblock_alloc(PAGE_2M_SIZE,PAGE_2M_SIZE);
             mem_set((void*)pa, 0, PAGE_2M_SIZE);
             memblock_mmap(kpml4t_ptr, pa, vmemmap_va,PAGE_ROOT_RW_2M1G, PAGE_2M_SIZE);
             vmemmap_va += PAGE_2M_SIZE;
