5e7a2750fa2777b9b2520659bc0661bab5f76350
wind3 <wind33333333@gmail.com>
2025-06-02 21:42:34 +0800

    20250209

--- kernel/uefi.c
+++ kernel/uefi.c
@@ -7,12 +7,16 @@
 #include "vmalloc.h"
 
 void init_efi_runtime_service(void) {
-    for (UINT32 i=0;i<efi_memmap.count;i++) {
+    for (UINT32 i = 0; i < efi_memmap.count; i++) {
         if (efi_memmap.mem_map[i].Type == EFI_RUNTIME_SERVICES_DATA) {
-            efi_memmap.mem_map[i].VirtualStart = (UINT64)pa_to_va(efi_memmap.mem_map[i].PhysicalStart);
-        } else{
-            efi_memmap.mem_map[i].VirtualStart = (UINT64)iomap(efi_memmap.mem_map[i].PhysicalStart,efi_memmap.mem_map[i].NumberOfPages << PAGE_4K_SHIFT,PAGE_4K_SIZE,PAGE_ROOT_RWX_4K);
+            efi_memmap.mem_map[i].VirtualStart = (UINT64) pa_to_va(efi_memmap.mem_map[i].PhysicalStart);
+        } else {
+            efi_memmap.mem_map[i].VirtualStart = (UINT64) iomap(efi_memmap.mem_map[i].PhysicalStart,
+                                                                efi_memmap.mem_map[i].NumberOfPages << PAGE_4K_SHIFT,
+                                                                PAGE_4K_SIZE,PAGE_ROOT_RWX_4K);
         }
     }
-    boot_info->gRTS->SetVirtualAddressMap(efi_memmap.count*boot_info->mem_descriptor_size,boot_info->mem_descriptor_size,boot_info->mem_descriptor_version,&efi_memmap.mem_map);
+    boot_info->gRTS->SetVirtualAddressMap(efi_memmap.count * boot_info->mem_descriptor_size,
+                                          boot_info->mem_descriptor_size, boot_info->mem_descriptor_version,
+                                          &efi_memmap.mem_map);
 }
