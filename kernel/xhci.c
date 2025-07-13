14b2fef6d87064fc316b65cb8b4763db91e32499
wind3 <wind33333333@gmail.com>
2025-07-13 17:17:26 +0800

    20250209

--- kernel/xhci.c
+++ kernel/xhci.c
@@ -1,4 +1,6 @@
 #include "xhci.h"
+
+#include "apic.h"
 #include "pcie.h"
 #include "printk.h"
 #include "slub.h"
@@ -9,6 +11,11 @@ INIT_TEXT void init_xhci(void) {
     disable_msi_x(xhci_dev->pcie_config_space);
     xhci_dev->bar[0] = set_bar(xhci_dev->pcie_config_space,0);
     xhci_dev->msi_x_table = get_msi_x_table(xhci_dev);
+    UINT64 msg_addr = rdmsr(IA32_APIC_BASE_MSR) & ~0xFFFUL;
+    xhci_dev->msi_x_table[0].msg_addr_lo = msg_addr;
+    xhci_dev->msi_x_table[0].msg_addr_hi = msg_addr >> 32;
+    xhci_dev->msi_x_table[0].msg_data = 0x40;
+    xhci_dev->msi_x_table[0].vector_control = 0;
 
-    xhci_cap_regs_t *xhci_cap = xhci_dev->bar[0];
+
 }
