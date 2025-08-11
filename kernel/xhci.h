1ee82c0b3665b805ab2f3caa4f3b1865adbd5b36
wind3 <wind33333333@gmail.com>
2025-08-11 22:02:13 +0800

    20250209

--- kernel/xhci.h
+++ kernel/xhci.h
@@ -366,8 +366,8 @@ typedef struct {
     xhci_erst_t     *erstba;      //事件环段表
     xhci_trb_t      *erdp;        //事件环基地址
     union {
-        xhci_device_context32_t  *dcbaap32; //设备上下文32字节
-        xhci_device_context64_t  *dcbaap64; //设备上下文64字节
+        xhci_device_context32_t  **dcbaap32; //设备上下文32字节
+        xhci_device_context64_t  **dcbaap64; //设备上下文64字节
     };
 } xhci_regs_t;
 
