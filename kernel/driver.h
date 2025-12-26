9922be9791d84bbe77c0269f681bd029d08b80c9
wind3 <wind33333333@gmail.com>
2025-12-26 21:53:17 +0800

    20251012

--- kernel/driver.h
+++ kernel/driver.h
@@ -7,8 +7,8 @@ struct device_t;
 typedef struct driver_t{
     char *name;                              // 驱动名：如 "xhci_pci" "usb_storage" "uas"
     struct bus_type_t  *bus;                  // 驱动属于哪个 bus（必须与设备 bus 一致才可能匹配）
-    int  (*probe)(struct device_t *dev);      // probe：当 match 成功后调用；负责初始化设备、分配 driver_data、开启中断/DMA 等
-    void (*remove)(struct device_t *dev);      // remove：设备移除/解绑时调用；负责关闭硬件、释放 driver_data
+    int  (*probe)(struct device_t *dev);      // probe：统一接口
+    void (*remove)(struct device_t *dev);      // remove：统一接口
     list_head_t bus_node;               // 挂到 bus->drv_list
 } driver_t;
 
