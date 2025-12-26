a55ce9b3d715f2f4201f57f5f30fc2b1ee76d07c
wind3 <wind33333333@gmail.com>
2025-12-26 20:29:29 +0800

    20251012

--- kernel/driver.c
+++ kernel/driver.c
@@ -7,12 +7,10 @@
 void driver_register(driver_t *drv) {
     bus_type_t *bus = drv->bus;
     list_add_head(&bus->drv_list,&drv->bus_node);
-    list_head_t *next_dev_node = bus->dev_list.next;
-    while (next_dev_node != &bus->dev_list) {
+    for (list_head_t *next_dev_node = bus->dev_list.next;next_dev_node != &bus->dev_list;next_dev_node = next_dev_node->next){
         device_t *dev = CONTAINER_OF(next_dev_node,device_t,bus_node);
-        if (bus->match(dev,drv) && !dev->drv) {
+        if (bus->match(dev,drv)) {
             bus->probe(dev);
         }
-        next_dev_node = next_dev_node->next;
     }
 }
