0524a3f64f676568f7de0498b6fe3146a72ee798
wind3 <wind33333333@gmail.com>
2026-02-20 13:11:23 +0800

    20251012

--- kernel/scsi.c
+++ kernel/scsi.c
@@ -1,8 +1,12 @@
 #include "scsi.h"
 
+//构造测试lun单元命令
 void scsi_build_test_unit_ready(scsi_task_t *task,uint8 lun,scsi_sense_t *sense) {
+    //构造scsi命令
     scsi_cdb_test_unit_t *cdb = kzalloc(sizeof(scsi_cdb_test_unit_t));
     cdb->opcode = SCSI_TEST_UNIT_READY;
+
+    //构造任务
     task->cdb = cdb;
     task->cdb_len = sizeof(scsi_cdb_test_unit_t);
     task->lun = lun;
