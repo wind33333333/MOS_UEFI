f844962dc33d9bc93f78fbab9439c99b7eea2066
wind3 <wind33333333@gmail.com>
2026-02-20 15:04:56 +0800

    20251012

--- kernel/scsi.c
+++ kernel/scsi.c
@@ -1,18 +1,64 @@
 #include "scsi.h"
 
-//构造测试lun单元命令
-void scsi_build_test_unit_ready(scsi_task_t *task,uint8 lun,scsi_sense_t *sense) {
-    //构造scsi命令
-    scsi_cdb_test_unit_t *cdb = kzalloc(sizeof(scsi_cdb_test_unit_t));
-    cdb->opcode = SCSI_TEST_UNIT_READY;
+#include "uas.h"
 
-    //构造任务
-    task->cdb = cdb;
-    task->cdb_len = sizeof(scsi_cdb_test_unit_t);
-    task->lun = lun;
-    task->dir = SCSI_DIR_NONE;
-    task->data_buf = NULL;
-    task->data_len = 0;
-    task->sense = sense;
+/**
+ * 发送 TEST UNIT READY 命令
+ */
+int32 scsi_test_unit_ready(void *dev_context,uint8 lun,void (*send_scsi_cmd_sync)(void*, scsi_task_t*)) {
+    //scsi_sense信息
+    scsi_sense_t sense;
+
+    //构造cdb
+    scsi_cdb_test_unit_t cdb = {
+        .opcode = SCSI_TEST_UNIT_READY,
+    };
+
+    //构造scsi_task
+    scsi_task_t task={.cdb = &cdb,
+        .cdb_len = sizeof(scsi_cdb_test_unit_t),
+        .lun = lun,
+        .dir = SCSI_DIR_NONE,
+        .data_buf = NULL,
+        .data_len = 0,
+        .sense = &sense,
+        .status = -1
+    };
+
+    //发送scsi命令
+    do {
+        send_scsi_cmd_sync(dev_context, &task);
+    } while (task.status == 2 && sense.flags_key == 0x6 && sense.asc == 0x29 && sense.ascq == 0);
+
+    return task.status;
+}
+
+
+//获取u盘信息
+int32 scsi_send_inquiry(void *dev_context,uint8 lun,void (*send_scsi_cmd_sync)(void*, scsi_task_t*)) {
+    scsi_sense_t sense;
+
+    scsi_cdb_inquiry_t cdb = {
+        .opcode = SCSI_INQUIRY,
+        .alloc_len = sizeof(scsi_inquiry_t)
+    };
+
+    scsi_inquiry_t *inquiry = kzalloc(sizeof(scsi_inquiry_t));
+
+    scsi_task_t task={
+        .cdb = &cdb,
+        .cdb_len = sizeof(scsi_cdb_inquiry_t),
+        .lun = lun,
+        .data_buf = inquiry,
+        .data_len = sizeof(scsi_inquiry_t),
+        .dir = SCSI_DIR_IN,
+        .sense = &sense,
+        .status = -1
+    };
+
+    send_scsi_cmd_sync(dev_context,&task);
+
+    kfree(inquiry);
+    return 0;
 }
 
