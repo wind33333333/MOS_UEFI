2d37577dcfe317bcab39acba821ed60af52a8e58
wind3 <wind33333333@gmail.com>
2026-09-12 18:27:25 +0800

    20251012

--- kernel/lib/printk.c
+++ kernel/lib/printk.c
@@ -376,7 +376,7 @@ INIT_TEXT void output_init(void) {
     Pos.YPosition = 0;
     Pos.XCharSize = 8;
     Pos.YCharSize = 16;
-    Pos.FB_addr = (uint32*)pa_to_va(tmp_boot_info->frame_buffer_base);
+    Pos.FB_addr = (uint32*)tmp_boot_info->frame_buffer_base;
     Pos.FB_length = tmp_boot_info->frame_buffer_size;
     Pos.lock = 0;
 
